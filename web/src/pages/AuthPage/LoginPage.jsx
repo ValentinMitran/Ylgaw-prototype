eeb57ccbc716f378abea9cb3bf3d7fa78539dbcd
Valentin Mitran <ToolsVM@pm.me>
2020-03-06 20:55:12 +0000

    Styling Auth Pages

--- web/src/pages/AuthPage/LoginPage.jsx
+++ web/src/pages/AuthPage/LoginPage.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./AuthPage.css";
 
-function LoginPage(){
-    return(<>Login Page</>);
+function LoginPage() {
+  return (
+    <>
+      <div className="authWrapper">
+        <div className="authFormContainer">
+          <img
+            src="https://raw.githubusercontent.com/ValentinMitran/Ylgaw/master/Ylgaw.png"
+            alt="Ylgaw Logo"
+          />
+          <form>
+            Username:
+            <input type="text" name="username" id="username" />
+            Password:
+            <input type="text" name="password" id="password" />
+            <button type="submit">Login</button>
+            <Link to="/register">Register</Link>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default LoginPage;
--- web/src/pages/AuthPage/RegisterPage.jsx
+++ web/src/pages/AuthPage/RegisterPage.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./AuthPage.css";
 
-function RegisterPage(){
-    return(<>Register Page</>);
+function RegisterPage() {
+  return (
+    <>
+      <div className="authWrapper">
+        <div className="authFormContainer">
+          <img
+            src="https://raw.githubusercontent.com/ValentinMitran/Ylgaw/master/Ylgaw.png"
+            alt="Ylgaw Logo"
+          />
+          <form>
+            Username:
+            <input type="text" name="username" id="username" />
+            Password:
+            <input type="text" name="password" id="password" />
+            <button type="submit">Register</button>
+            <Link to="/login">Login</Link>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default RegisterPage;
