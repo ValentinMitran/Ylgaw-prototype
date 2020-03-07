8c6a11dfdb32e02b17521799652c0c397a6f9509
Valentin Mitran <ToolsVM@pm.me>
2020-03-07 01:28:03 +0000

    Front-end Auth Core

--- web/src/pages/AuthPage/LoginPage.jsx
+++ web/src/pages/AuthPage/LoginPage.jsx
@@ -1,8 +1,33 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, Redirect } from "react-router-dom";
 import "./AuthPage.css";
 
 function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    localStorage.authToken ? setIsLoggedIn(true) : setIsLoading(false);
+  }, []);
+
+  const submitForm = async e => {
+    e.preventDefault();
+    console.log(username);
+    console.log(password);
+  };
+
+  if (isLoading) {
+    return (
+      <>
+        Loading...
+        {isLoggedIn ? <Redirect to="/" /> : null}
+      </>
+    );
+  }
+
   return (
     <>
       <div className="authWrapper">
@@ -11,11 +36,23 @@ function LoginPage() {
             src="https://raw.githubusercontent.com/ValentinMitran/Ylgaw/master/Ylgaw.png"
             alt="Ylgaw Logo"
           />
-          <form>
+          <form onSubmit={submitForm}>
             Username:
-            <input type="text" name="username" id="username" />
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
             Password:
-            <input type="text" name="password" id="password" />
+            <input
+              type="text"
+              name="password"
+              id="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
             <button type="submit">Login</button>
             <Link to="/register">Register</Link>
           </form>
