1609d61aa9a3bdae100edcd38297c0bc9367ba0b
Valentin Mitran <ToolsVM@pm.me>
2020-03-07 00:03:55 +0000

    Private Route JWT Verification

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,23 @@
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
+const verifyToken = require("../utils/verifyToken");
 
-router.get("/isLoggedIn", (req, res) => {
-  res.send(false);
+router.get("/isLoggedIn",verifyToken, (req, res) => {
+  res.send(true);
 });
+
+
+router.post("/register", (req,res) => {
+    res.send(true);
+});
+
+router.post("/login", (req,res) => {
+    res.send(true);
+});
+
+router.post("/logout", (req,res) => {
+    res.send(false);
+});
+
 
 module.exports = router;
