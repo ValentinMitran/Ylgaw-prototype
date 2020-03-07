cc9a3e80d61a91ab386cf5705f6514c4b305357a
Valentin Mitran <ToolsVM@pm.me>
2020-03-07 01:48:54 +0000

    Auth Data Validation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,23 +1,25 @@
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
 const verifyToken = require("../utils/verifyToken");
+const {
+  registerValidation,
+  loginValidation
+} = require("./../utils/dataValidation");
 
-router.get("/isLoggedIn",verifyToken, (req, res) => {
+router.get("/isLoggedIn", verifyToken, (req, res) => {
   res.send(true);
 });
 
-
-router.post("/register", (req,res) => {
-    res.send(true);
+router.post("/register", (req, res) => {
+  res.send(true);
 });
 
-router.post("/login", (req,res) => {
-    res.send(true);
+router.post("/login", (req, res) => {
+  res.send(true);
 });
 
-router.post("/logout", (req,res) => {
-    res.send(false);
+router.post("/logout", (req, res) => {
+  res.send(false);
 });
-
 
 module.exports = router;
