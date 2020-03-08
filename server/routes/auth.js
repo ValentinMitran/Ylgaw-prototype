d37eec7a3c6a942e5ba3c87c3c1bdb58c5bac8db
Valentin Mitran <ToolsVM@pm.me>
2020-03-08 13:19:03 +0000

    Register: Username Treatment

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -13,52 +13,52 @@ router.get("/isLoggedIn", verifyToken, (req, res) => {
 });
 
 router.post("/register", async (req, res) => {
-    const { error } = registerValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-  
-    //Check if email exists
-    const usernameExists = await User.findOne({ username: req.body.username });
-    if (usernameExists) return res.status(400).send("USERNAME ALREADY IN USE");
-  
-    //Hash passwords
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-  
-    //Create a new user
-    const user = new User({
-      username: req.body.username,
-      password: hashedPassword
-    });
-  
-    try {
-      const savedUser = await user.save();
-      res.send("Success");
-    } catch (err) {
-      res.status(400).send(err);
-    }
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const treatedUsername = req.body.username.toLowerCase().replace(/\s/g, "");
+
+  //Check if email exists
+  const usernameExists = await User.findOne({ username: treatedUsername });
+  if (usernameExists) return res.status(400).send("USERNAME ALREADY IN USE");
+
+  //Hash passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  //Create a new user
+  const user = new User({
+    username: treatedUsername,
+    password: hashedPassword
   });
+
+  try {
+    const savedUser = await user.save();
+    res.send("Success");
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 router.post("/login", async (req, res) => {
+  //LETS VALIDATE
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    //LETS VALIDATE
-    const { error } = loginValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+  //Check if email exists
+  const user = await User.findOne({ username: req.body.username });
+  if (!user) return res.status(400).send("Username doesnt exist");
 
-    //Check if email exists
-    const user = await User.findOne({ username: req.body.username });
-    if (!user) return res.status(400).send("Username doesnt exist");
+  //Password Check
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send("Invalid password");
 
-    //Password Check
-    const validPass = await bcrypt.compare(req.body.password, user.password);
-    if (!validPass) return res.status(400).send("Invalid password");
-
-    //Assign Token to logged user
-    const token = jwt.sign(
-      { _id: user._id, name: user.name },
-      process.env.JWT_TOKEN
-    );
-    res.header("authToken", token).send();
-  });
+  //Assign Token to logged user
+  const token = jwt.sign(
+    { _id: user._id, name: user.name },
+    process.env.JWT_TOKEN
+  );
+  res.header("authToken", token).send();
+});
 
 router.post("/logout", (req, res) => {
   res.send(false);
