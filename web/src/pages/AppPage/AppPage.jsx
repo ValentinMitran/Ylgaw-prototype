d651e45366a10ceea3c59b5dde0afe2102f9b724
Valentin Mitran <ToolsVM@pm.me>
2020-03-09 20:47:55 +0000

    Core Nav

--- web/src/pages/AppPage/AppPage.jsx
+++ web/src/pages/AppPage/AppPage.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 import Error404 from "./../ErrorPage/Error404";
 import './AppPage.css';
+import {MdHome,MdPerson,MdSearch} from 'react-icons/md';
+import {FiPlusSquare} from 'react-icons/fi';
+import {IoMdHeartEmpty} from 'react-icons/io';
 
 function AppPage() {
   return (
     <>
+      <nav className="coreNav">
+        <Link to="/u"> <MdHome/></Link>
+        <Link to="/u"> <MdSearch/></Link>
+        <Link to="/u"> <FiPlusSquare/></Link>
+        <Link to="/u"> <IoMdHeartEmpty/></Link>
+        <Link to="/u"> <MdPerson/></Link>
+      </nav>
+
       <Switch>
         <Route exact path="/">
+
           FIRST PAGE
+    
+
         </Route>
         <Route path="/u">user module</Route>
         <Route path="*">
