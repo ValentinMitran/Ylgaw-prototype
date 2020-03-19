2870815947952077bb742f7e0c586cad9f73b1d4
Valentin Mitran <ToolsVM@pm.me>
2020-03-19 16:40:32 +0000

    Time Machine: Upload Handler

--- server/routes/timeMachine.js
+++ server/routes/timeMachine.js
@@ -1,16 +1,32 @@
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
+const multer = require("multer");
 const verifyToken = require("../utils/verifyToken");
+const fs = require("fs");
+
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    const path = `./timemachine/${req.body["username"]}`;
+    fs.mkdirSync(path, { recursive: true });
+    cb(null, path);
+  },
+  filename: function(req, file, cb) {
+    var filename = file.originalname;
+    var fileExtension = filename.split(".")[1];
+    cb(null, Date.now() + "." + fileExtension);
+  }
+});
+const upload = multer({ storage: storage });
+
+router.post("/upload", upload.single("daily"), verifyToken, (req, res) => {
+  req.file ? res.send(true) : res.send(false);
+});
 
 router.post("/get", verifyToken, (req, res) => {
   if (req.body.date == 21) {
     res.send(true);
+  } else {
+    res.send(false);
   }
-  res.send(false);
 });
-
-router.post("/upload", verifyToken, (req, res) => {
-  res.send(false);
-});
-
 module.exports = router;
--- web/src/modules/TimeMachine/Uploader.jsx
+++ web/src/modules/TimeMachine/Uploader.jsx
@@ -1,24 +1,32 @@
 import React, { useEffect, useState } from "react";
 
-function Uploader(){
-    const [file, setFile] = useState(false);
+function Uploader() {
+  const handleImageUpload = async event => {
+    const files = event.target.files;
+    const formData = new FormData();
+    formData.append("username", "mrx");
 
-    const submitForm = async e => {
-      e.preventDefault();
-    };
-  
-    useEffect(() => {}, []);
+    formData.append("daily", files[0]);
+    let response = await fetch("/api/timeMachine/upload", {
+      method: "POST",
+      headers: {
+        "auth-token": localStorage.authToken
+      },
+      body: formData
+    }).catch(err => {
+      alert(err);
+    });
+    response = await response.text();
+    alert(response);
+  };
 
-    return(
+  useEffect(() => {}, []);
 
-
-        <form onSubmit={submitForm}>
-          <input type="file" name="file" id="" onChange={() => setFile(true)} />
-          <button type="submit" disabled={!file}>
-            Submit
-          </button>
-        </form>
-    );
+  return (
+    <form>
+      <input type="file" name="daily" id="" onChange={handleImageUpload} />
+    </form>
+  );
 }
 
 export default Uploader;
